77ba4b701e28df98522215ddbea47166a1b065cc
Arkadiy <arkadiy@ex.ua>
2018-07-10 00:35:00 +0300

    fixed deploy port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
     res.send('Marblewolf app running...');
 });
 
-app.listen(8080, function () {
-    console.log('App listening on port 8080!');
+app.listen(port, function () {
+    console.log('App listening on port ' + port + '!');
 });
